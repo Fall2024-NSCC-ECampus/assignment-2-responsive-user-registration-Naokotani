4a2d77243b53533cfdfcb214c3185bdd6becea82
Chris Hughes <naokotani@protonmail.com>
2024-10-27 13:52:39 -0300

    Updated bootsrap

--- rur/src/main/java/com/example/rur/controller/PublicController.java
+++ rur/src/main/java/com/example/rur/controller/PublicController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @Controller
 public class PublicController {
-
     final
     UserRepository userRepository;
 
--- rur/src/main/java/com/example/rur/controller/SecretController.java
+++ rur/src/main/java/com/example/rur/controller/SecretController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SecretController {
-
     @GetMapping("/secret")
     public String secret() {
         return "secret";
     }
-
 }
