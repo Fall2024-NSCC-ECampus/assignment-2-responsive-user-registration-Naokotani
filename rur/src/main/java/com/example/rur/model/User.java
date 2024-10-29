dc12414309b75c3d65f0f8d9f91a0e8a38acad72
Chris Hughes <naokotani@protonmail.com>
2024-10-28 21:04:34 -0300

    added annotation validations to user

--- rur/src/main/java/com/example/rur/model/User.java
+++ rur/src/main/java/com/example/rur/model/User.java
@@ -56,24 +56,8 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public void encodePassword() {
